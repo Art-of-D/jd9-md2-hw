8e2718449baadd7a32a13c29daa0e5bdbb64f0e9
Artur Didur <didur.art@gmail.com>
2023-03-14 10:47:53 +0200

    third commit

--- src/test/java/SumCalculatorTest.java
+++ src/test/java/SumCalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class SumTestOfSumCalculator {
+public class SumCalculatorTest {
     SumCalculator sums;
     @BeforeEach
     public void beforeEachMethod(){
